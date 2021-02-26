cb51c2ae87b2bddbe29f965df82c197085f910e4
Игорь <vasyukovin@yandex.ru>
2021-02-26 21:17:57 +0300

    Валидация полей формы

--- js/form.js
+++ js/form.js
@@ -1,8 +1,19 @@
-const price = document.getElementById('price');
+const title = document.getElementById('title');
 const type = document.getElementById('type');
+const price = document.getElementById('price');
 const timeForm = document.querySelector('.ad-form__element--time');
 const timeInSelector = timeForm.elements.timein;
 const timeOutSelector = timeForm.elements.timeout;
+const roomNumberOptions = document.getElementById('room_number');
+const capacityOptions = document.getElementById('capacity');
+
+const validateTitle = () => {
+  if (title.value.length < '30') {
+    title.setCustomValidity('Заголовок должен содержать больше ' + title.getAttribute('minlength') + ' символов');
+  } else {title.setCustomValidity('')}
+  title.reportValidity();
+}
+title.addEventListener('input', validateTitle)
 
 const setPriceValue = (priceValue) => {
   price.setAttribute('min', priceValue);
@@ -30,25 +41,35 @@ const setTypePrice = () => {
       break;
   }
 }
-
 setTypePrice();
-
 type.addEventListener('input', () => {
   setTypePrice();
+  price.reportValidity();
 })
+price.addEventListener('input', price.reportValidity)
 
 timeOutSelector.value = timeInSelector.value;
+timeInSelector.addEventListener('input', () => timeOutSelector.value = timeInSelector.value);
+timeOutSelector.addEventListener('input', () => timeInSelector.value = timeOutSelector.value);
 
-timeInSelector.addEventListener('input', () => {
-  timeOutSelector.value = timeInSelector.value;
-});
-
-timeOutSelector.addEventListener('input', () => {
-  timeInSelector.value = timeOutSelector.value;
-});
-
-
-
+const getValidCapacity = () => {
+  if (roomNumberOptions.value === '1' && (capacityOptions.value === '2' || capacityOptions.value === '3' || capacityOptions.value === '0')) {
+    capacityOptions.setCustomValidity('В 1 комнате может разместиться только 1 гость');
+  } else if (roomNumberOptions.value === '2' && (capacityOptions.value === '3' || capacityOptions.value === '0')) {
+    capacityOptions.setCustomValidity('В 2 комнатах может разместиться не больше 2 гостей');
+  } else if (roomNumberOptions.value === '3' && capacityOptions.value === '0') {
+    capacityOptions.setCustomValidity('В 3 комнатах может разместиться не больше 3 гостей');
+  } else if (roomNumberOptions.value === '100' && (capacityOptions.value === '1' || capacityOptions.value === '2' || capacityOptions.value === '3')) {
+    capacityOptions.setCustomValidity('Так это отель уже!');
+  } else {
+    capacityOptions.setCustomValidity('');
+  }
+  roomNumberOptions.reportValidity();
+  capacityOptions.reportValidity();
+}
+getValidCapacity();
+roomNumberOptions.addEventListener('input', getValidCapacity);
+capacityOptions.addEventListener('input', getValidCapacity);
 
 
 
