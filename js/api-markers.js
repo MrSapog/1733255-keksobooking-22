06d0421092050afa69870e4691910ee6615fbd65
Игорь <vasyukovin@yandex.ru>
2021-03-09 22:18:50 +0300

    Импортировал проверку всех фильтров

--- js/api-markers.js
+++ js/api-markers.js
@@ -1,6 +1,5 @@
 /* global L:readonly */
-import {checkType, checkPrice, checkRooms, checkGuests,
-  checkWifi, checkDishwasher, checkParking, checkWasher, checkElevator, checkConditioner} from './form.js';
+import {checkFilters} from './form.js';
 import {createRealtyAdsPopup} from './generation.js';
 import {map, pinIcon} from './map.js';
 
@@ -10,19 +9,7 @@ let markersLayer;
 const showRealtyAds = (realtyAds) => {
   let markersArray = [];
 
-  const filteredRealtyAds = realtyAds
-    .filter(ad =>
-      checkType(ad) &&
-      checkPrice(ad) &&
-      checkRooms(ad) &&
-      checkGuests(ad) &&
-      checkWifi(ad) &&
-      checkDishwasher(ad) &&
-      checkParking(ad) &&
-      checkWasher(ad) &&
-      checkElevator(ad) &&
-      checkConditioner(ad))
-    .slice(0, FILTERED_REALTY_ADS_COUNT);
+  const filteredRealtyAds = realtyAds.filter(checkFilters).slice(0, FILTERED_REALTY_ADS_COUNT);
 
   if (!filteredRealtyAds[0]) {
     return;
