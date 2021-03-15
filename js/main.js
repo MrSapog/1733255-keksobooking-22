0a92c663e36ba7cd61820868152c01ba0b18f3e8
Игорь <vasyukovin@yandex.ru>
2021-03-15 10:50:21 +0300

    переместил debounce

--- js/main.js
+++ js/main.js
@@ -5,20 +5,9 @@ import {showGetDataError} from './api-alerts.js';
 import {adForm, getData} from './api.js';
 import {filterForm} from './form.js';
 import {map, resetFormsAndMarkers} from './map.js';
+import {debounce} from './util.js';
 
 const RERENDER_DELAY = 500;
-
-const debounce = (cb, delay) => {
-  let timeoutId;
-  return (...args) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      timeoutId = null;
-      cb(...args);
-    }, delay)
-  }
-}
-
 const renderRealtyAds = () => getData(showRealtyAds, showGetDataError);
 const debouncedRealtyAdsRendering = debounce(renderRealtyAds, RERENDER_DELAY);
 
