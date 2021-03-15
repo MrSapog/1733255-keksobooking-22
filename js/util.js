2709900b48369f5c0f54674c1540ef29e8fcce40
Игорь <vasyukovin@yandex.ru>
2021-03-15 10:50:38 +0300

    добавил debounce и убрал ненужное

--- js/util.js
+++ js/util.js
@@ -1,37 +1,12 @@
-const getRandom = function(min, max) {
-  // Меняем местами параметры, если первый больше второго https://stackoverflow.com/questions/16201656/how-to-swap-two-variables-in-javascript
-  if (min > max) {
-    [min, max] = [max, min]
+const debounce = (cb, delay = 500) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      timeoutId = null;
+      cb(...args);
+    }, delay)
   }
-  // Превращаем в положительные https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
-  min = Math.abs(Math.ceil(min));
-  max = Math.abs(Math.floor(max));
-  // Часть функции с сайта MDN https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const getRandomFloat = function(min, max, digits) {
-  if (min > max) {
-    [min, max] = [max, min]
-  }
-  min = Math.abs(min);
-  max = Math.abs(max);
-  return (Math.random() * (max - min) + min).toFixed(digits);
-}
-
-const getRandomItem = function (someString) {
-  return someString[getRandom(1, someString.length - 1)]
-}
-
-const getRandomCombination = function (array) {
-  let randomCombinations = array.map(feature => {
-    if (getRandom(0, 1)) {
-      return;
-    }
-    return feature;
-  });
-  randomCombinations = randomCombinations.filter(feature => feature !== undefined);
-  return randomCombinations;
-}
-
-export {getRandom, getRandomFloat, getRandomItem, getRandomCombination};
+export {debounce};
